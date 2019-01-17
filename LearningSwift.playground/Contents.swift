707d3863f9d494577c3f850a3b72add26e6c3666
DEEPTI SHARMA <deepti.mcs17.du@gmail.com>
2019-01-17 23:57:29 +0530

    Page 9

--- LearningSwift.playground/Contents.swift
+++ LearningSwift.playground/Contents.swift
@@ -20,3 +20,44 @@ let height = 5.2
 // let statement = label + height <-------- Binary operator cannot be applied to String+Double , so conversion is necessary
 let statement = label + String(height)
 
+//E:3
+// using \() to include fractions and strings
+let number1 = 1.1
+let number2 = 3.4
+var namVariable  = "Deepti"
+let statement2 = "Hey I calculated this \( number1+number2 ) "
+let statement3 = "Hey \(namVariable) "
+
+//Multiline String
+let quotation = """
+                Do I have to try this as well ???
+                What I am 3 year old at programming.
+                Great .
+                """
+quotation
+print( quotation )
+
+// Arrays and Dictionaries
+var newArray = ["Deeps" , "Hey Deeps", "Helloo", "How are you?", "Fine"]
+newArray[0]
+
+var newDictionary = [
+    "Deepti" : "M.Sc.",
+    "Vipin" : "MCA",
+]
+newDictionary["Deepti"]
+newDictionary["Smrity"] = "MCA"
+
+print(newDictionary)
+
+
+newArray.append("Good Morning")
+print(newArray)
+
+//empty Dictionaries and Arrays
+var newArray2 = [String]()
+var newDict2 = [Float: String]()
+// or
+newArray2 = []
+newDict2 = [:]
+
