b44383cec52a6fc9ab7f65165b860d6c627e1e1d
DEEPTI SHARMA <deepti.mcs17.du@gmail.com>
2019-01-24 21:53:05 +0530

    Completed the Language Tour.

--- LearningSwift.playground/Contents.swift
+++ LearningSwift.playground/Contents.swift
@@ -736,4 +736,61 @@ print(fridgeIsOpen)
 
 // GENERICS
 
+func makeArray<Item>(repeating item:Item, numberOfTimes: Int) -> [Item] {
+    // whatever item is passed generically to the function, it works on the item
+    var result = [Item]()
+    for _ in 0..<numberOfTimes {
+        result.append(item)
+    }
+    return result
+}
+
+makeArray(repeating: "knock", numberOfTimes: 4)
+makeArray(repeating: 3, numberOfTimes: 4)
+
+// generics of functions, methods, classes, structure, enumeration
+
+// Reimplementation of Swift's standard library's optional type
+enum OptionalValue<Wrapped> {
+    case none
+    case some(Wrapped)
+}
+
+var possibleInteger: OptionalValue<Int> = .none
+possibleInteger = .some(100)
+var anotherValue: OptionalValue<String> = .some("My")
+var possibleDouble : OptionalValue<Double> = .some(3.14)
+
+
+// where : given right before the body to specify a list of reqrmnts : to require the type to implement a protocol , to require two types to be the same or to require class to have a particular superclass
+
+func anyCommonElements< T: Sequence, U: Sequence >(_ lhs: T, _ rhs: U) -> Bool where T.Element: Equatable, T.Element == U.Element {
+    for lhsItem in lhs {
+        for rhsItem in rhs {
+            if lhsItem == rhsItem {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+anyCommonElements([7.14,3.5], [3.5])
+
+// Experiment : 19
+
+func getCommonElements< T: Sequence, U: Sequence >(_ lhs: T, _ rhs: U) -> [T.Element] where T.Element: Equatable, T.Element == U.Element {
+    var result:[T.Element] = []
+    for lhsItem in lhs {
+        for rhsItem in rhs {
+            if lhsItem == rhsItem {
+                result.append(lhsItem)
+            }
+        }
+    }
+    return result
+}
+
+getCommonElements([7.14,3.5], [3.5])
+
 
