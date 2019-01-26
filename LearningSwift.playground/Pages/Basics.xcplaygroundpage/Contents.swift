c89c31314201fda78d10f6137f489a49ad6d2fe4
DEEPTI SHARMA <deepti.mcs17.du@gmail.com>
2019-01-27 00:31:02 +0530

    Basics : Page 84

--- LearningSwift.playground/Pages/Basics.xcplaygroundpage/Contents.swift
+++ LearningSwift.playground/Pages/Basics.xcplaygroundpage/Contents.swift
@@ -10,32 +10,60 @@
 
 
 // CONSTANTS AND VARIABLES
-
 // value and particular type are associated
-
 let anotherConstant = 10
 var anotherVariable = 0
-
 // multiple declaration
  var x=10.0, y=19.4, z=0.0
 
 // TYPE ANNOTATIONS
-
 // annotate a type to indicate the type of the value variable can store : String
 var welcomeMesage: String = "I am here."
-welcomeMesage = "Hello"
-
+welcomeMesage = "Hello World"
 // Multiple Declarations: annotated type
-var red, blue, green: Double
+var red:Double = 4.5 , blue:Double = 6.7 , green:Double = 9.4 //<=== This requires attention
+// green = 5.6
+// blue = 7.6
 
 // NAMING CONSTANTS AND VARIABLES
 // name can contain any character
 var 🍔: String = "This burger contains AT"
-
 // naming converntion : unicode character + no spacing + any character + no ( mathematical symbols + arrows+ private use unicode scalar values (??) + line- or box- drawing characters ) + cannot begin with numbers , though they may occur anywhere
 // redeclaration of variables not allowed , var of certain types cant change value just like constants
 // var -> const or vice versa conversion not possible
+// NOTE : giving a keywors or constant same name as a reserved keyword, surround it by (`) <- backticks
+//var (')var(') = 45
 
-// NOTE : 
+// PRINTING
+print(welcomeMesage)
+// print function has two parameters : separators , terminators : terminate at line break (default)
+print(welcomeMesage, terminator: "!!!!")
+// string interpolation : \() <-  to surpass datatype and print the content in the string
+print ("Here is an example : \(red)")
+
+// COMMENTS
+// use comments to include non executable text in code , ignored by compiler
+// single line : //  or multi line : /*....*/ : nested comments are possible
+
+// SEMICOLON
+// semicolons can be written after each statement
+let broco = "🥦" ; print(broco)
+
+
+// INTEGER
+// no fractional component , can be signed or unsigned
+// UInt8 : 8 bit unsigned integer
+// Int32 : 32 bit signed
+// 2 properties :  min and max
+let minValue = UInt8.min
+let maxValue = UInt8.max // + 1 <-  overflow
+
+// Int : Int32, Int64
+// print(Int.max)
+// UInt : UInt32, UInt64
+
+
+// FLOATING-POINT NUMBERS
+
 
 //: [Next](@next)
