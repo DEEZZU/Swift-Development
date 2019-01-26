68bb3ee9057f9d658b5cbfe1ffd74df7a860def1
DEEPTI SHARMA <deepti.mcs17.du@gmail.com>
2019-01-26 18:28:59 +0530

    the basics page : 76

--- LearningSwift.playground/Pages/Basics.xcplaygroundpage/Contents.swift
+++ LearningSwift.playground/Pages/Basics.xcplaygroundpage/Contents.swift
@@ -1,4 +1,41 @@
 //: [Previous](@previous)
+// fundamental types : Int, Double, Float, Bool, String
+// Collection types : Array, Set, Dictionary
+// Variables : to store data , refered to identify values
+// Constants : variables whose values cannot be changed , more powerful >??
+// makes code safer and clearer in intent
+// Advanced Types : Tuples , enables to create and pass arounf groupings of values : multiple value return as a single compound
+// Optionals : handle the absence of the value  : value or nil : can work for any type not just classes like in obj-C : more expressive, safer
+// TYPE-SAFE Language : helps you to be clear of type of value the code can use . helps within the development process
 
+
+// CONSTANTS AND VARIABLES
+
+// value and particular type are associated
+
+let anotherConstant = 10
+var anotherVariable = 0
+
+// multiple declaration
+ var x=10.0, y=19.4, z=0.0
+
+// TYPE ANNOTATIONS
+
+// annotate a type to indicate the type of the value variable can store : String
+var welcomeMesage: String = "I am here."
+welcomeMesage = "Hello"
+
+// Multiple Declarations: annotated type
+var red, blue, green: Double
+
+// NAMING CONSTANTS AND VARIABLES
+// name can contain any character
+var 🍔: String = "This burger contains AT"
+
+// naming converntion : unicode character + no spacing + any character + no ( mathematical symbols + arrows+ private use unicode scalar values (??) + line- or box- drawing characters ) + cannot begin with numbers , though they may occur anywhere
+// redeclaration of variables not allowed , var of certain types cant change value just like constants
+// var -> const or vice versa conversion not possible
+
+// NOTE : 
 
 //: [Next](@next)
